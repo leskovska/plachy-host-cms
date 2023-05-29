4b8986c2edf5c877e5240d7e403b2755c1d4c6bd
michaela.leskovska <michaela.leskovska@o2.cz>
2023-05-29 20:27:05 +0200

    fix: dont show form for main image when there is not a model to save relationship to

--- resources/views/livewire/introduction/index.blade.php
+++ resources/views/livewire/introduction/index.blade.php
@@ -2,28 +2,26 @@
     <x-slot name="header">
         <h2>{{ $page_title }}</h2>
     </x-slot>
-    <div>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                    <h3>Úvodní obrázek</h3>
-                    @if($introduction)
-                        <div>
-                            {{ $introduction->getFirstMedia('introduction') }}
-                        </div>
-                    @endif
-                    <div class="py-6 text-gray-900">
-                        <x-primary-button
+    @if($introduction)
+        <div>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <h3>Úvodní obrázek</h3>
+                        <div>{{ $introduction->getFirstMedia('introduction') }}</div>
+                        <div class="py-6 text-gray-900">
+                            <x-primary-button
                                 onClick="Livewire.emit('openModal',
-                                    'introduction.image-form',
-                                    'Vložit obrázek')"
-                        >Změnit úvodní obrázek</x-primary-button>
+                                        'introduction.image-form',
+                                        'Vložit obrázek')"
+                            >Změnit úvodní obrázek
+                            </x-primary-button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
     <div>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
